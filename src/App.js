9948252deb6bc2eb963f4b17e957c9d234d141b5
Denys-Fadieiev <dencheek0911@gmail.com>
2023-01-17 21:19:15 +0200

    add slides data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.module.scss';
 import Slider from './components/Slider';
 
-function App() {
+function App(props) {
+  const slides = [
+    {
+      title: 'title',
+      description: 'description',
+      src: 'https://i.infocar.ua/i/2/5429/107528/1920x.jpg',
+      alt: 'alt',
+    },
+    {
+      title: 'title2',
+      description: 'description2',
+      src: 'https://i.infocar.ua/i/2/5429/107529/1920x.jpg',
+      alt: 'alt',
+    },
+    {
+      title: 'title3',
+      description: 'description3',
+      src: 'https://i.infocar.ua/i/2/5429/107533/1920x.jpg',
+      alt: 'alt',
+    },
+    {
+      title: 'title4',
+      description: 'description4',
+      src: 'https://i.infocar.ua/i/2/5429/107534/1920x.jpg',
+      alt: 'alt',
+    },
+  ];
+
   return (
     <article>
-      <Slider/>
+      <Slider slides={slides}/>
     </article>
   );
 }
