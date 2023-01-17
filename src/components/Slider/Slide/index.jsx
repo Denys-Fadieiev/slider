ed7c823913c5dbfa165aadd95e0a9177ee0bb3c6
Denys-Fadieiev <dencheek0911@gmail.com>
2023-01-17 21:25:18 +0200

    add Slide component

--- src/components/Slider/Slide/index.jsx
+++ src/components/Slider/Slide/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from './Slide.module.scss';
 
-const Slide = () => {
+const Slide = ({slide: { title, description, src, alt}}) => {
   return (
     <div>
-      Slide
+      <button>X</button>
+      <figure >
+        <img 
+          className={styles.img}
+          src={src} 
+          alt={alt}/>
+        <figcaption>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </figcaption>
+      </figure>
     </div>
   );
 }
