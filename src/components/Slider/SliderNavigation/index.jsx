fcbd7b1234db5194ebfaeec106907c2083b5acb9
Denys-Fadieiev <dencheek0911@gmail.com>
2023-01-19 23:46:52 +0200

    add SliderNavigation component

--- src/components/Slider/SliderNavigation/index.jsx
+++ src/components/Slider/SliderNavigation/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {MdNavigateNext, MdNavigateBefore} from 'react-icons/md';
+import styles from './SliderNavigation.module.scss';
 
 const SliderNavigation = ({handleNextSlide, handlePrevSlide} ) => {
   return (
-    <div>
-      <button onClick={() => handlePrevSlide()}>{'<'}</button>
-      <button onClick={() => handleNextSlide()}>{'>'}</button>
+    <div className={styles.wrapperNavigation}>
+      <button 
+        className={styles.btnNav}
+        onClick={() => handlePrevSlide()}>
+        {<MdNavigateBefore/>}
+      </button>
+      <button 
+        className={styles.btnNav}
+        onClick={() => handleNextSlide()}
+      >
+        {<MdNavigateNext/>}
+      </button>
     </div>
   );
 }
