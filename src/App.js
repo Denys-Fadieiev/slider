f258bcc29a09efff2057e29ba74cf79129c8769c
Denys-Fadieiev <dencheek0911@gmail.com>
2023-01-19 23:45:49 +0200

    add Slider component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,69 @@
-import './App.module.scss';
 import Slider from './components/Slider';
 
 function App(props) {
   const slides = [
     {
-      title: 'title',
-      description: 'description',
-      src: 'https://i.infocar.ua/i/2/5429/107528/1920x.jpg',
+      title: 'Lorem Ipsum is simply dummy',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_1.jpg',
       alt: 'alt',
     },
     {
-      title: 'title2',
-      description: 'description2',
-      src: 'https://i.infocar.ua/i/2/5429/107529/1920x.jpg',
+      title: 'Lorem Ipsum ',
+      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_2.jpg',
       alt: 'alt',
     },
     {
-      title: 'title3',
-      description: 'description3',
-      src: 'https://i.infocar.ua/i/2/5429/107533/1920x.jpg',
+      title: 'Lorem Ipsum is',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_3.jpg',
       alt: 'alt',
     },
     {
-      title: 'title4',
-      description: 'description4',
-      src: 'https://i.infocar.ua/i/2/5429/107534/1920x.jpg',
+      title: 'Lorem Ipsum is simply dummy',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_4.jpg',
+      alt: 'alt',
+    },
+    {
+      title: 'Lorem Ipsum',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_5.jpg',
+      alt: 'alt',
+    },
+    {
+      title: 'Lorem Ipsum is simply dummy',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_6.jpg',
+      alt: 'alt',
+    },
+    {
+      title: 'Lorem Ipsum is...',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_7.jpg',
+      alt: 'alt',
+    },
+    {
+      title: 'Lorem Ipsum is simply dummy',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_8.jpg',
+      alt: 'alt',
+    },
+    {
+      title: 'Lorem Ipsum...',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      src: 'https://ukravto.ua/files/avto/brand/Jeep/compass/exterior/190225_JEEP_COMPASS_S_9.jpg',
       alt: 'alt',
     },
   ];
 
+
+
   return (
-    <article>
+    <>
       <Slider slides={slides}/>
-    </article>
+    </>
   );
 }
 
--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,31 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Slide from "./Slide";
 import SliderNavigation from "./SliderNavigation";
 import SliderControlPanel from "./SliderControlPanel";
+import styles from './Slider.module.scss';
 
 const Slider = ({ slides }) => {
+  const slidesLenght = slides.length;
 	const [currentSlide, setCurrentSlide] = useState(0);
+  const [stepTime, setStepTime] = useState(5000);
+  const [isAutoPlay, setIsAutoPlay] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  // const fullscreenContent = cx(styles.content, {
+  //   [styles.contentFullscreen] : isFullScreen,
+  //   }
+  // )
 
 	const handleNextSlide = () => 
-    currentSlide < (slides.length - 1) 
+    currentSlide < (slidesLenght - 1) 
     ? setCurrentSlide(currentSlide => currentSlide + 1) 
     : setCurrentSlide(currentSlide => 0);
 
 	const handlePrevSlide = () => 
       currentSlide > 0
 			? setCurrentSlide(currentSlide => currentSlide - 1)
-			: setCurrentSlide(currentSlide => slides.length - 1);
+			: setCurrentSlide(currentSlide => slidesLenght - 1);
 
+  const handleAutoPlay = () => setIsAutoPlay(!isAutoPlay);
+
+  const handleFullScreen = () => setIsFullScreen(!isFullScreen);
+
+  useEffect(()=>  {
+    let timerId = null;
+    if(isAutoPlay && !timerId) {
+      timerId = setTimeout(() => handleNextSlide(), stepTime);
+    };
+    return () => {
+      clearTimeout(timerId);
+      timerId = null;
+    };
+  }, [isAutoPlay, stepTime, currentSlide]);
 
 	return (
-		<article>
-			<Slide slide={slides[currentSlide]}/>
-			<SliderNavigation 
-        handleNextSlide={handleNextSlide}
-        handlePrevSlide={handlePrevSlide} 
-      />
-			<SliderControlPanel />
-		</article>
+    <div className={`${!isFullScreen ? styles.content : styles.contentFullscreen}`}>
+      <article className={styles.slyder}>
+        <Slide 
+          slide={slides[currentSlide]}
+          isHovered={[isHovered, setIsHovered]}
+          isFullScreen={[isFullScreen, setIsFullScreen]}
+          handleFullScreen={handleFullScreen}
+        />
+        <SliderNavigation 
+          handleNextSlide={handleNextSlide}
+          handlePrevSlide={handlePrevSlide}
+          isHovered={[isHovered, setIsHovered]}
+        />
+        <SliderControlPanel
+          slidesLenght={slidesLenght}
+          currentSlide={[currentSlide, setCurrentSlide]}
+          isAutoPlay={[isAutoPlay, setIsAutoPlay]}
+          isFullScreen={[isFullScreen, setIsFullScreen]}
+          stepTime={[stepTime, setStepTime]}
+          isHovered={[isHovered, setIsHovered]}
+          handleAutoPlay={handleAutoPlay}
+          handleFullScreen={handleFullScreen}
+        />
+      </article>
+    </div>
 	);
 };
 
