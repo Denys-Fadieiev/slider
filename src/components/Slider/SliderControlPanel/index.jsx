76d9f4d8caf40e8727bcec78ed5d235610de8df9
Denys-Fadieiev <dencheek0911@gmail.com>
2023-01-19 23:47:15 +0200

    add SliderControl Panel component

--- src/components/Slider/SliderControlPanel/index.jsx
+++ src/components/Slider/SliderControlPanel/index.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import { HiPlay, HiPause } from "react-icons/hi";
+import { BiFullscreen, BiExitFullscreen } from "react-icons/bi";
+import styles from "./SliderControlPanel.module.scss";
 
-const SliderControlPanel = () => {
+const SliderControlPanel = ({
+  slidesLenght,
+  currentSlide: [currentSlide, setCurrentSlide],
+	isAutoPlay: [isAutoPlay, setIsAutoPlay],
+	isFullScreen: [isFullScreen, setIsFullScreen],
+	stepTime: [stepTime, setStepTime],
+	isHovered: [isHovered, setIsHovered],
+	handleAutoPlay,
+	handleFullScreen,
+}) => {
+	
   return (
-    <div>
-      SliderControlPanel
-    </div>
-  );
-}
+		<div className={`${!isFullScreen ? styles.wrapperControlPanel : styles.wrapperControlPanelFullscreen}`}>
+			<div className={styles.wrapper}>
+				<button className={`${!isFullScreen ? styles.btn : styles.btnFullscreen}`} onClick={handleAutoPlay}>
+					{!isAutoPlay ? <HiPlay /> : <HiPause />}
+				</button>
+				<input
+          className={styles.input}
+					type="number"
+					min={1}
+					max={10}
+					value={stepTime / 1000}
+					onChange={({ target: { value } }) => {
+						setStepTime(+value * 1000);
+					}}
+				/>
+			</div>
+      <p className={styles.num}>{currentSlide + 1}/{slidesLenght}</p>
+			<button className={`${!isFullScreen ? styles.btn : styles.btnFullscreen}`} onClick={handleFullScreen}>
+				{!isFullScreen ? <BiFullscreen /> : <BiExitFullscreen />}
+			</button>
+		</div>
+	);
+};
 
 export default SliderControlPanel;
